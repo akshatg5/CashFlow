083aed76b1950681dee0bbb92cb408d410fa1b7e
akshatg5 <akshatgirdhar5@gmail.com>
2024-04-07 18:38:33 +0530

    Debugging

--- backend/Routes/index.js
+++ backend/Routes/index.js
@@ -2,11 +2,9 @@ const express = require("express");
 const router = express.Router();
 const userRouter = require("./user");
 const accountRouter = require("./account");
-const stocksRouter = require("./stocks");
 
 // all the user related routes are located in the userRouter
 router.use("/user", userRouter);
 router.use("/account", accountRouter);
-router.use("/stocks",stocksRouter)
 
 module.exports = router;
